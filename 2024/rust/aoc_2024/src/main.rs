e29d7739f3440c4c95681265792e6f71e4d92c2c
lehaas <lennart.haas@outlook.com>
2024-12-01 13:45:53 +0100

    Modularize main.rs.

--- 2024/rust/aoc_2024/src/main.rs
+++ 2024/rust/aoc_2024/src/main.rs
@@ -3,15 +3,12 @@ mod day_01;
 use day_01::{solve_part_1, solve_part_2};
 
 fn main() {
-    println!(
-        "Result (small): {}",
-        solve_part_1("../../data/day_01_small.txt")
-    );
-    println!("Result: {}", solve_part_1("../../data/day_01.txt"));
+    let task_small_path = "../../data/day_01_small.txt";
+    let task_path = "../../data/day_01.txt";
 
-    println!(
-        "Result (small): {}",
-        solve_part_2("../../data/day_01_small.txt")
-    );
-    println!("Result: {}", solve_part_2("../../data/day_01.txt"));
+    println!("Result (small): {}", solve_part_1(task_small_path));
+    println!("Result: {}", solve_part_1(task_path));
+
+    println!("Result (small): {}", solve_part_2(task_small_path));
+    println!("Result: {}", solve_part_2(task_path));
 }
