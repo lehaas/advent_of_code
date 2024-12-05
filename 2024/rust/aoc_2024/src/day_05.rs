8b11ec537a79446641805f2d1f63992d2933f67e
lehaas <lennart.haas@outlook.com>
2024-12-05 22:35:11 +0100

    Implement Day 5.

---
+++ 2024/rust/aoc_2024/src/day_05.rs
@@ -0,0 +1,79 @@
+use core::iter::Iterator;
+use std::collections::HashSet;
+use std::fs;
+use std::path::Path;
+
+/// Read the content of a file into a String.
+fn read_file_content(file_path: impl AsRef<Path>) -> String {
+    let content: String =
+        fs::read_to_string(file_path).expect("Should have been able to read the file");
+    content
+}
+
+fn is_valid(vec: &Vec<i32>, rules: &HashSet<(i32, i32)>) -> bool {
+    vec.iter().enumerate().all(|(i, &value)| {
+        vec[i + 1..]
+            .iter()
+            .all(|&other| !rules.contains(&(other, value)))
+    })
+}
+
+fn split_content(content: &str) -> (&str, &str) {
+    content.split_once("\n\n").expect("Invalid content format")
+}
+
+fn parse_rules(rules_unparsed: &str) -> HashSet<(i32, i32)> {
+    rules_unparsed
+        .lines()
+        .map(|line| {
+            line.split_once("|")
+                .map(|(x, y)| (x.parse::<i32>().unwrap(), y.parse::<i32>().unwrap()))
+                .unwrap()
+        })
+        .collect()
+}
+
+// Sort according to rules, i.e., for (x, y) in rules, x has to be before y.
+fn sort_by_rules(mut vec: Vec<i32>, rules: &HashSet<(i32, i32)>) -> Vec<i32> {
+    vec.sort_by(|&a, &b| {
+        if rules.contains(&(a, b)) {
+            std::cmp::Ordering::Less // `a` should come before `b`
+        } else if rules.contains(&(b, a)) {
+            std::cmp::Ordering::Greater // `b` should come before `a`
+        } else {
+            std::cmp::Ordering::Equal // No specific order for `a` and `b`
+        }
+    });
+    vec
+}
+
+pub fn solve_part_1(path: &str) -> i32 {
+    let content = read_file_content(path);
+    let (rules_unparsed, prints) = split_content(&content);
+    let rules = parse_rules(&rules_unparsed);
+
+    prints
+        .lines()
+        .map(|line| {
+            line.split(",")
+                .map(|num| num.parse::<i32>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .filter(|vec| is_valid(vec, &rules))
+        .map(|vec| vec[vec.len() / 2])
+        .sum()
+}
+pub fn solve_part_2(path: &str) -> i32 {
+    let content = read_file_content(path);
+
+    let (rules_unparsed, prints) = split_content(&content);
+    let rules = parse_rules(&rules_unparsed);
+
+    prints
+        .lines()
+        .map(|line| line.split(",").map(|c| c.parse::<i32>().unwrap()).collect())
+        .filter(|vec| !is_valid(vec, &rules))
+        .map(|vec| sort_by_rules(vec, &rules))
+        .map(|vec| vec[vec.len() / 2])
+        .sum()
+}
--- 2024/rust/aoc_2024/src/main.rs
+++ 2024/rust/aoc_2024/src/main.rs
@@ -1,9 +1,9 @@
-mod day_04;
-use day_04::{solve_part_1, solve_part_2};
+mod day_05;
+use day_05::{solve_part_1, solve_part_2};
 
 fn main() {
-    let task_small_path = "../../data/day_04_small.txt";
-    let task_path: &str = "../../data/day_04.txt";
+    let task_small_path = "../../data/day_05_small.txt";
+    let task_path: &str = "../../data/day_05.txt";
 
     println!("Result (small): {}", solve_part_1(task_small_path));
     println!("Result: {}", solve_part_1(task_path));
