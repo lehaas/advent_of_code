088d6137e1e8013dde4d9b97f48c152b320f6037
lehaas <lennart.haas@outlook.com>
2024-12-06 14:49:06 +0100

    Implment Day 6.

--- 2024/rust/aoc_2024/src/day_06.rs
+++ 2024/rust/aoc_2024/src/day_06.rs
@@ -9,15 +9,15 @@ fn read_file_content(file_path: impl AsRef<Path>) -> String {
     content
 }
 
-fn find_start_position(map: &Vec<Vec<char>>) -> (usize, usize) {
+fn find_start_position(map: &Vec<Vec<char>>) -> Option<(usize, usize)> {
     for (i, line) in map.iter().enumerate() {
         for (j, &value) in line.iter().enumerate() {
             if value == '^' {
-                return (i, j);
+                return Some((i, j));
             }
         }
     }
-    panic!("Start position not found.")
+    None
 }
 
 fn get_next_direction(
@@ -41,53 +41,113 @@ fn get_new_position(pos: (usize, usize), direction: (i32, i32)) -> (usize, usize
     )
 }
 
-fn count_pluses(map: Vec<Vec<char>>) -> i32 {
+fn count_minuses(map: &Vec<Vec<char>>) -> i32 {
     map.iter()
-        .map(|line| line.iter().filter(|&&c| c == '+').count() as i32)
+        .map(|line| line.iter().filter(|&&c| c == '-' || c == '+').count() as i32)
         .sum()
 }
-fn count_steps(mut map: Vec<Vec<char>>, mut pos: (usize, usize)) -> i32 {
+
+#[derive(Debug, PartialEq)]
+enum State {
+    LoopFound,
+    ExitedMap,
+}
+
+fn found_loop(pos: (usize, usize), direction: (i32, i32), map: &Vec<Vec<char>>) -> bool {
+    let new_pos: (usize, usize) = get_new_position(pos, direction);
+    map[pos.0][pos.1] == '+' && map[new_pos.0][new_pos.1] == '#'
+}
+
+fn predict_guard_path(map: &mut Vec<Vec<char>>, mut pos: (usize, usize)) -> State {
     let mut directions = [(-1, 0), (0, 1), (1, 0), (0, -1)].iter().cycle();
     let mut direction: (i32, i32) = get_next_direction(&mut directions);
 
     loop {
-        map[pos.0][pos.1] = '+';
+        if found_loop(pos, direction, &map) {
+            return State::LoopFound;
+        }
+        map[pos.0][pos.1] = '-';
 
         if !is_within_bounds(pos, direction, &map) {
             break;
         }
 
-        let new_pos = get_new_position(pos, direction);
+        let mut new_pos: (usize, usize) = get_new_position(pos, direction);
 
-        match map[new_pos.0][new_pos.1] {
-            '#' => direction = get_next_direction(&mut directions),
-            '.' | '^' | '+' => pos = new_pos,
-            _ => panic!("Unknown charcter"),
+        while map[new_pos.0][new_pos.1] == '#' {
+            map[pos.0][pos.1] = '+';
+            direction = get_next_direction(&mut directions);
+            new_pos = get_new_position(pos, direction);
         }
+        pos = new_pos;
     }
-    // map.iter().for_each(|line| println!("{:?}", line));
 
-    count_pluses(map)
+    State::ExitedMap
+}
+
+fn print_map(map: &Vec<Vec<char>>) {
+    map.iter()
+        .for_each(|line: &Vec<char>| println!("{:?}", line));
 }
 
 pub fn solve_part_1(path: &str) -> i32 {
     let content = read_file_content(path);
 
-    let map = content
+    let mut map = content
         .lines()
         .map(|line| line.chars().collect::<Vec<char>>())
         .collect::<Vec<_>>();
-    let pos = find_start_position(&map);
-    count_steps(map, pos)
+    let start_position = find_start_position(&map).expect("Guard position not found");
+    predict_guard_path(&mut map, start_position);
+    count_minuses(&map)
 }
 
 pub fn solve_part_2(path: &str) -> i32 {
     let content = read_file_content(path);
 
-    let map = content
+    let original_map = content
         .lines()
         .map(|line| line.chars().collect::<Vec<char>>())
         .collect::<Vec<_>>();
-    let pos = find_start_position(&map);
-    count_steps(map, pos)
+    let start_position = find_start_position(&original_map).expect("Guard position not found");
+
+    let blockage_positions = determine_blockag_start_points(&original_map, start_position);
+
+    blockage_positions
+        .iter()
+        .filter(|(i, j)| blockage_creates_loop(&original_map, i, j, start_position))
+        .count() as i32
+}
+
+fn blockage_creates_loop(
+    original_map: &Vec<Vec<char>>,
+    i: &usize,
+    j: &usize,
+    start_position: (usize, usize),
+) -> bool {
+    let mut map: Vec<Vec<char>> = original_map.clone();
+    map[*i][*j] = '#';
+    predict_guard_path(&mut map, start_position) == State::LoopFound
+}
+
+fn determine_blockag_start_points(
+    original_map: &Vec<Vec<char>>,
+    start_position: (usize, usize),
+) -> Vec<(usize, usize)> {
+    let mut tracked_map = original_map.clone();
+    assert!(predict_guard_path(&mut tracked_map, start_position) == State::ExitedMap);
+    let blockage_positions: Vec<_> = tracked_map
+        .iter()
+        .enumerate()
+        .flat_map(|(i, line)| {
+            line.iter().enumerate().filter_map(move |(j, &value)| {
+                if value == '+' || value == '-' {
+                    Some((i, j))
+                } else {
+                    None
+                }
+            })
+        })
+        .collect();
+    blockage_positions
 }
--- 2024/rust/aoc_2024/src/main.rs
+++ 2024/rust/aoc_2024/src/main.rs
@@ -8,6 +8,6 @@ fn main() {
     println!("Result (small): {}", solve_part_1(task_small_path));
     println!("Result: {}", solve_part_1(task_path));
 
-    // println!("Result (small): {}", solve_part_2(task_small_path));
-    // println!("Result: {}", solve_part_2(task_path));
+    println!("Result (small): {}", solve_part_2(task_small_path));
+    println!("Result: {}", solve_part_2(task_path));
 }
