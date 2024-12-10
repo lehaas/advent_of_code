36a42ec7a8c0ca9f3e54ad5f294eaeaa5c44a11c
lehaas <lennart.haas@outlook.com>
2024-12-10 07:48:01 +0100

    Implement day 10.

---
+++ 2024/rust/aoc_2024/src/day_10.rs
@@ -0,0 +1,133 @@
+use core::iter::Iterator;
+use std::collections::HashSet;
+use std::fs;
+use std::path::Path;
+
+/// Read the content of a file into a String.
+fn read_file_content(file_path: impl AsRef<Path>) -> String {
+    let content: String =
+        fs::read_to_string(file_path).expect("Should have been able to read the file");
+    content
+}
+
+fn print_map<T: std::fmt::Debug>(map: &Vec<Vec<T>>) {
+    map.iter().for_each(|line: &Vec<T>| println!("{:?}", line));
+}
+pub fn solve_part_1(path: &str) -> usize {
+    let content = read_file_content(path);
+
+    let map: Vec<Vec<u32>> = parse_map(content);
+
+    let mut targets: Vec<Vec<HashSet<(usize, usize)>>> =
+        vec![vec![HashSet::new(); map[0].len()]; map.len()];
+
+    for i in (0..map.len()) {
+        for j in (0..map[0].len()) {
+            if map[i][j] == 9 {
+                targets[i][j].insert((i, j));
+            }
+        }
+    }
+
+    print_map(&targets);
+    for k in (0..9).rev() {
+        for i in 0..map.len() {
+            for j in 0..map[0].len() {
+                if map[i][j] != k {
+                    continue;
+                }
+                if i > 0 && (map[i - 1][j] == k + 1) {
+                    let t = targets[i - 1][j].clone();
+                    targets[i][j].extend(t);
+                }
+                if i < map.len() - 1 && (map[i + 1][j] == k + 1) {
+                    let t = targets[i + 1][j].clone();
+                    targets[i][j].extend(t);
+                }
+                if j > 0 && (map[i][j - 1] == k + 1) {
+                    let t = targets[i][j - 1].clone();
+                    targets[i][j].extend(t);
+                }
+                if j < map[0].len() - 1 && (map[i][j + 1] == k + 1) {
+                    let t = targets[i][j + 1].clone();
+                    targets[i][j].extend(t);
+                }
+            }
+        }
+    }
+
+    print_map(&targets);
+
+    (0..map.len())
+        .map(|i| {
+            (0..map[0].len())
+                .filter(|&j| map[i][j] == 0)
+                .map(|j| targets[i][j].len())
+                .sum::<usize>()
+        })
+        .sum()
+}
+
+pub fn solve_part_2(path: &str) -> usize {
+    let content = read_file_content(path);
+
+    let map = parse_map(content);
+    print_map(&map);
+
+    let mut targets: Vec<Vec<usize>> = vec![vec![0; map[0].len()]; map.len()];
+
+    for i in 0..map.len() {
+        for j in 0..map[0].len() {
+            if map[i][j] == 9 {
+                targets[i][j] = 1;
+            }
+        }
+    }
+
+    print_map(&targets);
+    for k in (0..9).rev() {
+        for i in 0..map.len() {
+            for j in 0..map[0].len() {
+                if map[i][j] != k {
+                    continue;
+                }
+                if i > 0 && (map[i - 1][j] == k + 1) {
+                    targets[i][j] += targets[i - 1][j];
+                }
+                if i < map.len() - 1 && (map[i + 1][j] == k + 1) {
+                    targets[i][j] += targets[i + 1][j];
+                }
+                if j > 0 && (map[i][j - 1] == k + 1) {
+                    targets[i][j] += targets[i][j - 1];
+                }
+                if j < map[0].len() - 1 && (map[i][j + 1] == k + 1) {
+                    targets[i][j] += targets[i][j + 1];
+                }
+            }
+        }
+    }
+
+    print_map(&targets);
+
+    (0..map.len())
+        .map(|i| {
+            (0..map[0].len())
+                .filter(|&j| map[i][j] == 0)
+                .map(|j| targets[i][j])
+                .sum::<usize>()
+        })
+        .sum()
+}
+
+fn parse_map(content: String) -> Vec<Vec<u32>> {
+    let mut map = content
+        .lines()
+        .map(|line| {
+            line.chars()
+                .into_iter()
+                .filter_map(|x| x.to_digit(10))
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    map
+}
--- 2024/rust/aoc_2024/src/main.rs
+++ 2024/rust/aoc_2024/src/main.rs
@@ -1,9 +1,9 @@
-mod day_09;
-use day_09::{solve_part_1, solve_part_2};
+mod day_10;
+use day_10::{solve_part_1, solve_part_2};
 
 fn main() {
-    let task_small_path = "../../data/day_09_small.txt";
-    let task_path: &str = "../../data/day_09.txt";
+    let task_small_path = "../../data/day_10_small.txt";
+    let task_path: &str = "../../data/day_10.txt";
 
     println!("Result (small): {}", solve_part_1(task_small_path));
     println!("Result: {}", solve_part_1(task_path));
